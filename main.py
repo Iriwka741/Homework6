0be13c1eec83d86b3ef3cf9382d578b84edab10f
Lenovo <katashirina89@gmail.com>
2023-10-10 22:26:41 +0300

    Implemented second task

--- main.py
+++ main.py
@@ -3,12 +3,28 @@
 
 #1.Напишіть функцію, яка обчислює добуток елементів списку цілих. Список передається як параметр.
 
-def multiply(b):
-    multiplication = 1
-    for elements in b:
-        if elements != 0:
-            multiplication *= elements
-    return multiplication
+#def multiply(b):
+  #  multiplication = 1
+   # for elements in b:
+     #   if elements != 0:
+     #       multiplication *= elements
+   # return multiplication
+
+#a = [1,2,3,4,5]
+#print(multiply(a))
+
+##############################
+#2.Напишіть функцію для знаходження мінімуму у списку цілих
+
+def _MinList(lst):
+  if len(lst) > 0:
+     _min = lst[0]
+
+  for i in range(1,len(lst)):
+   if lst[i] < _min:
+     _min = lst[i]
+
+  return _min
 
 a = [1,2,3,4,5]
-print(multiply(a))
+print(_MinList(a))
