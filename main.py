9856bd2d17ac03241974fe10d815ac474469bf9b
Lenovo <katashirina89@gmail.com>
2023-10-10 22:06:29 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #print("Hello teacher")
-print("Homework Banul")
+#print("Homework Banul")
+
+#1.Напишіть функцію, яка обчислює добуток елементів списку цілих. Список передається як параметр.
+
+def multiply(b):
+    multiplication = 1
+    for elements in b:
+        if elements != 0:
+            multiplication *= elements
+    return multiplication
+
+a = [1,2,3,4,5]
+print(multiply(a))
