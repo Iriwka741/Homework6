f8453ae4e8de4be25f57e94c58cb64189f3c9268
Lenovo <katashirina89@gmail.com>
2023-10-10 22:52:38 +0300

    Implemented third task

--- main.py
+++ main.py
@@ -16,15 +16,37 @@
 ##############################
 #2.Напишіть функцію для знаходження мінімуму у списку цілих
 
-def _MinList(lst):
-  if len(lst) > 0:
-     _min = lst[0]
+#def _MinList(lst):
+ # if len(lst) > 0:
+  #   _min = lst[0]
 
-  for i in range(1,len(lst)):
-   if lst[i] < _min:
-     _min = lst[i]
+ # for i in range(1,len(lst)):
+  # if lst[i] < _min:
+  #   _min = lst[i]
 
-  return _min
+  #return _min
 
-a = [1,2,3,4,5]
-print(_MinList(a))
+#a = [1,2,3,4,5]
+#print(_MinList(a))
+
+#####################################
+
+#3.Напишіть функцію, яка визначає кількість простих чисел у списку цілих
+
+def is_prime(num):
+  if num <= 1:
+    return False
+
+  for i in range(2, int(num**0.5) + 1):
+    if num % i == 0:
+      return False
+
+  return True
+
+def count_primes_in_list(lst):
+  count = 0
+
+  for num in lst:
+    if is_prime(num):
+     count += 1
+  return count
